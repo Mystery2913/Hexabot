13f9fd0c91afc9a34a75bdb09782a72e51198d7b
Dylan Sharm <mystery2913@gmail.com>
2022-05-17 10:05:53 +1200

    Fixed algorithm wih correct math
    Was calculting wrong change in x and y value for O calculation

--- testRig/testRig.py
+++ testRig/testRig.py
@@ -1,12 +1,14 @@
 import math
 import time
-from board import SCL, SDA
-import busio
+#from board import SCL, SDA
+#import busio
 
 # Import the PCA9685 module. Available in the bundle and here:
 #   https://github.com/adafruit/Adafruit_CircuitPython_PCA9685
 from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
+
+"""
 
 i2c = busio.I2C(SCL, SDA)
 
@@ -26,6 +28,8 @@ servo0.angle = 70
 servo1.angle = 130
 servo2.angle = 82
 
+"""
+
 arm1AngleC = 43
 arm2AngleC = 150
 arm1PointX = -117.017
@@ -33,12 +37,14 @@ arm1PointY = 57.939
 arm1Length = 140
 arm2Length = 90
 
-targetX = 0
-targetY = 0
+targetX = 20
+targetY = -30
 
-ac = math.sqrt((targetX-arm1PointX)**2+(targetY-arm1PointY)**2)
+acx = (arm1PointX-targetX)
+acy = (arm1PointY-targetY)
+ac = math.sqrt(acx**2+acy**2)
 B = math.acos((ac**2-arm2Length**2-arm1Length**2)/(-2*arm2Length*arm1Length))
-O = math.atan(abs(arm1PointY)/abs(arm1PointX))
+O = math.atan(abs(acy)/abs(acx))
 A = math.asin(arm1Length*math.sin(B)/(ac))
 
 B = math.degrees(B)
@@ -50,5 +56,5 @@ a = A-O
 servo0angle = arm1AngleC + (180 - B)
 servo1angle = arm2AngleC - a
 
-servo0.angle = servo0angle
-servo1.angle = servo1angle
+#servo0.angle = servo0angle
+#servo1.angle = servo1angle
